5fd81fd739fe049c627cc2bbcd06fa3ada0dd726
Oleksiy Pyltsov <valar999@gmail.com>
2024-07-06 23:37:41 +0200

    Modify scripts/run_agent.ts
    To run the agent and show runId.

---
+++ scripts/run_agent.ts
@@ -0,0 +1,28 @@
+import { Agent__factory } from '../typechain-types';
+
+async function main() {
+  if (!process.env.CONTRACT_ADDRESS) {
+    throw new Error("CONTRACT_ADDRESS env variable is not set.");
+  }
+  if (!process.env.TWITTER_LOGIN) {
+    throw new Error("TWITTER_LOGIN env variable is not set.");
+  }
+
+  const contractAddress = process.env.CONTRACT_ADDRESS;
+  const [signer] = await ethers.getSigners();
+
+  const contract = Agent__factory.connect(contractAddress, signer);
+
+  const login = process.env.TWITTER_LOGIN;
+  const transactionResponse = await contract.runAgent(login);
+  const receipt = await transactionResponse.wait();
+  console.log(`Run Id: ${receipt.logs[1].args[0]}`);
+  console.log(`Tx: ${receipt.hash}`);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
