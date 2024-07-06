171161736393a3bca7f5b233e474a0d454232180
Oleksiy Pyltsov <valar999@gmail.com>
2024-07-06 16:07:57 +0200

    Implement basic test for func response

--- contracts/Agent.sol
+++ contracts/Agent.sol
@@ -2,8 +2,34 @@
 pragma solidity ^0.8.24;
 
 import "./interfaces/IOracle.sol";
+import "hardhat/console.sol";
 
 contract Agent {
+    mapping(uint => User) public users;
+
+    struct User {
+	string id;
+	string login;
+	Tweet[] tweets;
+    }
+
+    struct Tweet {
+	uint256 userId;
+	uint256 tweetId;
+	string text;
+	uint256 timestamp;
+    }
+
+    uint private agentRunCount;
+    mapping(uint => AgentRun) public agentRuns;
+    struct AgentRun {
+        address owner;
+	string twitterLogin;
+        IOracle.Message[] messages;
+        uint iteration;
+        bool is_finished;
+    }
+
     // @notice Address of the contract owner
     address private owner;
 
@@ -12,9 +38,6 @@ contract Agent {
 
     // @notice Last response received from the oracle
     string public lastResponse;
-
-    // @notice Counter for the number of calls made
-    uint private callsCount;
 
     // @notice Event emitted when the oracle address is updated
     event OracleAddressUpdated(address indexed newOracleAddress);
@@ -44,10 +67,17 @@ contract Agent {
         emit OracleAddressUpdated(newOracleAddress);
     }
 
-    function runAgent() public returns (uint) {
-        uint currentId = callsCount;
-        callsCount = currentId + 1;
+    function runAgent(string memory twitterLogin) public returns (uint) {
+        AgentRun storage run = agentRuns[agentRunCount];
 
+        run.owner = msg.sender;
+	run.twitterLogin = twitterLogin;
+
+	uint currentId = agentRunCount;
+        agentRunCount++;
+	console.log("Agent run created with id: %d", currentId);
+
+	console.log("Creating function call: {}", oracleAddress);
         IOracle(oracleAddress).createFunctionCall(
             currentId,
             "code_interpreter",
@@ -57,6 +87,7 @@ contract Agent {
 	    "d = requests.get('https://api.twitter.com/2/users/by?usernames=alex', headers={'Authorization': 'Bearer '+token}).json();"
 	    "print(d['data'][0]['id']);"
         );
+	// emit RunCreated(run.id, run.owner, run.iteration);
 
         return currentId;
     }
@@ -66,10 +97,48 @@ contract Agent {
         string memory response,
         string memory errorMessage
     ) public onlyOracle {
+	console.log("Received response: %s", response);
         if (keccak256(abi.encodePacked(errorMessage)) != keccak256(abi.encodePacked(""))) {
             lastResponse = errorMessage;
         } else {
             lastResponse = response;
         }
+    }
+
+    // @notice Handles the response from the oracle for an OpenAI LLM call
+    // @param runId The ID of the agent run
+    // @param response The response from the oracle
+    // @param errorMessage Any error message
+    // @dev Called by teeML oracle
+    function onOracleOpenAiLlmResponse(
+        uint runId,
+        IOracle.OpenAiResponse memory response,
+        string memory errorMessage
+    ) public onlyOracle {
+	/*
+        AgentRun storage run = agentRuns[runId];
+
+        if (!compareStrings(errorMessage, "")) {
+            IOracle.Message memory newMessage = createTextMessage("assistant", errorMessage);
+            run.messages.push(newMessage);
+            run.responsesCount++;
+            run.is_finished = true;
+            return;
+        }
+        if (run.responsesCount >= run.max_iterations) {
+            run.is_finished = true;
+            return;
+        }
+        if (!compareStrings(response.content, "")) {
+            IOracle.Message memory newMessage = createTextMessage("assistant", response.content);
+            run.messages.push(newMessage);
+            run.responsesCount++;
+        }
+        if (!compareStrings(response.functionName, "")) {
+            IOracle(oracleAddress).createFunctionCall(runId, response.functionName, response.functionArguments);
+            return;
+        }
+        run.is_finished = true;
+	*/
     }
 }
--- test/Agent.ts
+++ test/Agent.ts
@@ -1,21 +1,46 @@
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import { expect } from "chai";
 
 describe("Agent", function () {
   async function deploy() {
-    const owner = await ethers.getSigners()[0];
+    const [owner] = await ethers.getSigners();
 
-    const Oracle = await ethers.getContractFactory("ChatOracle");
+    const Oracle = await ethers.getContractFactory("Oracle");
     const oracle = await Oracle.deploy();
 
     const Agent = await ethers.getContractFactory("Agent");
-    const agent = await Agent.deploy("0x0000000000000000000000000000000000000000");
+    const agent = await Agent.deploy(oracle.target);
+    await agent.setOracleAddress(oracle.target);
 
     return {agent, oracle, owner};
   }
 
-  describe("Deployment", function () {
-    it("Should be run", async function () {
-      console.log("We will build our first Galadriel project!");
+  describe("Temporary", function () {
+    /*
+    it("test", async function () {
+      const {agent, oracle, owner} = await loadFixture(deploy);
+      const addUserTx = await agent.temp_addUser(1234, "alex");
+      await addUserTx.wait();
+      console.log(await agent.users(1234));
+      console.log(await agent.users(1235));
+    });
+    */
+
+    it("User can start agent run", async () => {
+      const {agent, oracle, owner} = await loadFixture(deploy);
+      await agent.setOracleAddress(oracle.target);
+      await oracle.updateWhitelist(owner.address, true);
+
+      await agent.runAgent("VitalikButerin");
+      await oracle.addFunctionResponse(0, 0, "test respo", "");
+      // const messages = await oracle.getMessagesAndRoles(0, 0);
+      // console.log(messages);
+      /*
+      const messages = await oracle.getMessagesAndRoles(0, 0)
+      expect(messages.length).to.equal(2)
+      expect(messages[0].content[0].value).to.equal("system prompt")
+      expect(messages[1].content[0].value).to.equal("which came first: the chicken or the egg?")
+      */
     });
   });
 });
